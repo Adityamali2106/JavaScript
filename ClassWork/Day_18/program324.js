a7be0561f1fa39591c5665e56d83643b07d3c828
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-03-26 14:55:38 +0530

    Add files via upload

---
+++ ClassWork/Day_18/program324.js
@@ -0,0 +1,3 @@
+console.log("Jay Ganesh...");
+
+console.log("Marvelloys Infosystem");
---
+++ ClassWork/Day_18/program350.js
@@ -0,0 +1,9 @@
+function main() {
+  let iValue = 721;
+  console.log(iValue); // 721
+  console.log(typeof iValue); // number
+  console.log(iValue % 10);
+  console.log(Math.floor(iValue / 10));
+}
+
+main();
